14b52449860a05c02949d071aa5a0ce78a83fe73
Sofiyyah1 <sofiyattaiwo139@gmail.com>
2022-08-24 21:06:57 -0700

    correct Task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for c in range(97, 123):
-    if chr(c) == "q" or chr(c) == "e":
-        print("", end="")
-    else:
+    if chr(c) != "q" and chr(c) != "e":
     	print("{:s}".format(chr(c)), end="")
