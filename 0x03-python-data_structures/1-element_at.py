e6a52087b13a68a56d25c80c46bfaf83158e2931
Sofiyyah1 <sofiyattaiwo139@gmail.com>
2022-08-29 14:42:21 -0700

    Add Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 def element_at(my_list, idx):
     if idx < 0:
-        return None
+        return (None)
     elif idx > (len(my_list) - 1):
-        return None
+        return (None)
     return(my_list[idx])
